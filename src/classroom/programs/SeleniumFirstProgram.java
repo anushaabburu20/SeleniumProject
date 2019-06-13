a110304c827731a542451fb15bafed79ece931cf
Anusha Abburu <anushaabburu@madhusudans-mbp.home>
2019-06-13 15:59:46 -0500

    Removed comment line

--- src/classroom/programs/SeleniumFirstProgram.java
+++ src/classroom/programs/SeleniumFirstProgram.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SeleniumFirstProgram {
 
 	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "/Users/anushaabburu/eclipse-workspace/SeleniumProject/BrowserDrivers/chromedriver 2");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://www.google.com");
